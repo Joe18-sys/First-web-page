873cf83c9c7f0470aa1e70ae780fd75a2c675c8e
Joe18-sys <joensitejadamartinez@gmail.com>
2026-04-25 15:32:31 -0400

    Update login-script.js

--- login-script.js
+++ login-script.js
@@ -4,19 +4,19 @@ document.addEventListener('DOMContentLoaded', () => {
     loginForm.addEventListener('submit', (e) => {
         e.preventDefault();
 
-        // 1. Obtener lo que el usuario escribió en el login
+        
         const userIngresado = document.querySelector('.input2').value;
         const passIngresada = document.querySelector('.input3').value;
 
-        // 2. Traer los datos que guardamos en la página de registro
+        
         const datosGuardados = JSON.parse(localStorage.getItem('usuarioRegistrado'));
 
-        // 3. VALIDACIÓN
+        
         if (datosGuardados) {
-            // Comparamos usuario Y contraseña
+           
             if (userIngresado === datosGuardados.usuario && passIngresada === datosGuardados.clave) {
                 alert(`¡Bienvenido de nuevo, ${datosGuardados.nombre}!`);
-                window.location.href = "index.html"; // ENTRAR
+                window.location.href = "index.html"; 
             } else {
                 alert("❌ Usuario o contraseña incorrectos.");
             }
